7e7aa2169ad3c74d08c2baaa23b001ffa93c49ff
Roman Sultani <inatlusnamor@gmail.com>
2025-08-10 13:48:46 -0700

    Tuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "@/styles/"; // adjust path as needed
+import "@/styles"; // adjust path as needed
 import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
